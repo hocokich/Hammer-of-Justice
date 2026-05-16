e7456d2034c5a471ad9c9553aeaa08cf425fbcb9
Sanyok <bondarenco65@gmail.com>
2026-05-17 00:43:46 +0700

    BG update
    Полностью переработал логику паралакс фона.
    
    Теперь это префаб привязанный к камере. Также, уменьшил ширину картинок. Теперь все работает как конфетка.
    
    Также, префаб фона был добавлен во все сцены. Фон Грид был переработан, чтобы было видно фон с паралаксом.

--- scripts/GameManager/GameManager.cs
+++ scripts/GameManager/GameManager.cs
@@ -14,8 +14,8 @@ public class GameManager : MonoBehaviour
 
 	void Awake()
 	{
-		Application.targetFrameRate = 60;
-
+		Application.targetFrameRate = (int)Screen.currentResolution.refreshRateRatio.value;
+		 
 		if (Instance != null)
 		{
 			Destroy(gameObject);
